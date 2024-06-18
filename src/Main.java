0d61207727bf5bb3732e70ebf5a6ffc84bef1a76
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2024-06-18 15:39:43 -0300

    Inserido todos os funcionários na mesma ordem da tabela informada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+import entidades.Funcionario;
+
+import javax.swing.text.DateFormatter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 3.1 Inserir todos os funcionários na mesma ordem da tabela
+        List<Funcionario> listaDeFuncionarios = new ArrayList<>();
+        String caminhoDaPastaDoProjeto = System.getProperty("user.dir");
+        String caminhoDoArquivo = caminhoDaPastaDoProjeto + "/src/anexo/tabela-funcionarios.csv";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("O Sistema Operacional é Windows? S/N");
+        String opcao = sc.next();
+        if (opcao.toLowerCase().equals("s")) {
+            caminhoDoArquivo = caminhoDoArquivo.replace("/", "\\");
+        }
+        sc.close();
+        DateTimeFormatter formatadorDeData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoDoArquivo))) {
+            br.readLine();
+            String umaLinha = br.readLine();
+            while (umaLinha != null) {
+                String[] linha = umaLinha.split(",");
+                String nome = linha[0];
+                LocalDate dataNascimento = LocalDate.parse(linha[1], formatadorDeData);
+                BigDecimal salario = new BigDecimal(linha[2]);
+                String funcao = linha[3];
+                Funcionario funcionario = new Funcionario(nome, dataNascimento, salario, funcao);
+                listaDeFuncionarios.add(funcionario);
+                umaLinha = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Nome\tData Nascimento\tSalário\t\tFunção");
+        for (Funcionario f : listaDeFuncionarios) {
+            System.out.println(f);
+        }
+
+
     }
 }
